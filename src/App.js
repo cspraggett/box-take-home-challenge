9d33dd5956d21a91dbc33149538a1aac3d3a22a6
cspraggett <cspraggett@gmail.com>
2020-04-07 16:10:48 -0400

    style box and add flex-box support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 import { DragDropContext } from "react-beautiful-dnd";
 
 import ItemList from "./components/ItemList";
+import Box from "./components/Box";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
 
 const initialItems = [
   { id: 1, weight: 1 },
@@ -11,13 +19,18 @@ const initialItems = [
   { id: 4, weight: 8 },
 ];
 
+const initialBox = [{ name: "box1", maxWeight: 10 }];
+
 function App() {
   const onDragEnd = React.useCallback(() => {
     console.log("drag end");
   }, []);
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <ItemList items={initialItems} />
+      <Container>
+        <ItemList items={initialItems} />
+        <Box box={initialBox} />
+      </Container>
     </DragDropContext>
   );
 }
