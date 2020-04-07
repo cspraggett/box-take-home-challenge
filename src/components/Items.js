0e0817c6367c786007b406fb06bc89ebede269a5
cspraggett <cspraggett@gmail.com>
2020-04-07 15:48:13 -0400

    make items stand out

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,13 +2,21 @@ import React from "react";
 import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
 
+const Container = styled.div`
+  border: 1px solix lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: lightgreen;
+`;
+
 export default function Items({ id, weight }) {
   console.log("in Items:", id, weight);
 
   return (
     <Draggable draggableId={id.toString()} index={id}>
       {(provided, snapshot) => (
-        <div
+        <Container
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
@@ -16,7 +24,7 @@ export default function Items({ id, weight }) {
           <li>
             Name: {id} Weight: {weight}
           </li>
-        </div>
+        </Container>
       )}
     </Draggable>
   );
